3be71f61dc35a55ef050dabb8cd71ec25fa023c4
austin17h <2020ahuang@tjhsst.edu>
2019-12-09 01:18:23 -0500

    made changes to optical_flow.0.py. Now background subtraction algorithm is significantly faster with the combination of dxdy.

--- research-austin/code/optical_flow.0.py
+++ research-austin/code/optical_flow.0.py
@@ -30,18 +30,30 @@ def averageBGR(frame1):
 
     return [average_b, average_g, average_r]
 
-def background_subtractor(testnpArray1, frame1, average_b1, average_g1, average_r1, background_thresholder1):
-    for a in range(frame1.shape[0]):
-        for j in range(frame1.shape[1]):
+def background_subtractor(frame1, average_b1, average_g1, average_r1, background_thresholder1, from_x, to_x, from_y, to_y):
+    testnpArray1 = np.zeros((720, 1280, 3), np.uint8)
+    for a in range(from_y, to_y):
+        for j in range(from_x, to_x):
             k = frame1[a, j]
             distance = math.sqrt( (average_r1-k[0])**2 + (average_g1-k[1])**2 + (average_b1-k[2])**2 )
-            #print(distance) #try threshold of 70
+            #print(distance) #try threshold of 90
             if(distance >= background_thresholder1):
                 testnpArray1[a][j] = (255,255,255)
             else:
                 testnpArray1[a][j] = (0, 0, 0)
 
     return testnpArray1
+
+def synthesis(dxdy_bbox_input, bs_bbox_input, frame):
+    actual_bbox = bs_bbox_input
+    """#if the two boxes are close enough, trust the background subtractor
+    if (abs(dxdy_bbox_input[0]-bs_bbox_input[0]) <= dxdy_bbox_input[2] and abs(dxdy_bbox_input[1]-bs_bbox_input[1]) <= dxdy_bbox_input[3]):
+        actual_bbox = bs_bbox_input
+    #if the two boxes are separate, trust the dxdy bbox input then need to reset the tracker (assuming pass continues)
+    elif (abs(dxdy_bbox_input[0]-bs_bbox_input[0]) > dxdy_bbox_input[2] or abs(dxdy_bbox_input[1]-bs_bbox_input[1]) > dxdy_bbox_input[3]):
+        actual_bbox = dxdy_bbox_input"""
+
+    return actual_bbox
 
 def main():
     filename = input("Enter full path name to video")
@@ -70,14 +82,30 @@ def main():
     #print(average_g)
     #print(average_b)
 
-    ## APPLY BACKGROUND THRESHOLD ##
-    testnpArray = background_subtractor(testnpArray, frame, average_b, average_g, average_r, background_thresholder)
-
-    elapsed_time = time.time() - start_time
-    print(elapsed_time)
-
     ## INIT TRACKING FOR 1ST FRAME
     bbox = (275, 185, 10, 20) #initial bounding box
+
+    #set boundaries to try apply background subtractor (region should place bbox in the center of a 3x3 grid)
+    to_x = frame[0]
+    to_y = frame[1]
+    from_x = 0
+    from_y = 0
+    if (bbox[0] - bbox[2] > 0):
+        from_x = bbox[0] - bbox[2]
+    if (bbox[1] - bbox[3] > 0):
+        from_y = bbox[1] - bbox[3]
+    if (bbox[0] + 2*bbox[2] < frame.shape[0]):
+        to_x = bbox[0] + 2*bbox[2]
+    if (bbox[1] + 2*bbox[3] < frame.shape[1]):
+        to_y = bbox[1] + 2*bbox[3]
+
+    ## APPLY BACKGROUND THRESHOLD ##
+    testnpArray = background_subtractor(frame, average_b, average_g, average_r, background_thresholder, from_x, to_x, from_y, to_y) #0, frame.shape[0], 0, frame.shape[1])
+    #cv2.rectangle(testnpArray, (from_x, from_y), (to_x, to_y), (255, 0, 0))
+    cv2.imshow("testnpArray", testnpArray)
+    cv2.waitKey(0)
+    elapsed_time = time.time() - start_time
+    print(elapsed_time)
     trackerType = "CSRT"
     tracker = cv2.TrackerCSRT_create()
     tracker.init(testnpArray, bbox)
@@ -96,7 +124,10 @@ def main():
       [average_b, average_g, average_r] = averageBGR(frame)
 
       ## BACKGROUND SUBTRACTION
-      testnpArray = background_subtractor(testnpArray, frame, average_b, average_g, average_r, background_thresholder)
+      testnpArray = background_subtractor(frame, average_b, average_g, average_r, background_thresholder, from_x, to_x, from_y, to_y)
+      cv2.rectangle(testnpArray, (from_x, from_y), (to_x, to_y), (255, 0, 0))
+      cv2.imshow("testnpArray", testnpArray)
+      cv2.waitKey(0)
       if dxdy == (0,0): #base the first delta value off of background subtractor
             success, bbox = tracker.update(testnpArray)
             new_center = (bbox[0]+bbox[2]/2, bbox[1]+bbox[3]/2)
@@ -104,14 +135,35 @@ def main():
             center = new_center
       else: #test value of old delta plus position and goal is to weigh this greater than background subtractor
           # or if tracker gives location much more varied than previous delta, than invalidate background subtractor result
+
+            #guess using dxdy
             attempted_center = tuple(np.rint(np.add(center, dxdy)).astype(int))
-            attempted_bbox = (int(new_center[0]-bbox[2]/2), int(new_center[1]-bbox[3]/2), int(bbox[2]), int(bbox[3]))
+            attempted_bbox = (int(attempted_center[0]-bbox[2]/2), int(attempted_center[1]-bbox[3]/2), int(bbox[2]), int(bbox[3]))
+
+            #guess using background subtraction
+            success, bbox = tracker.update(testnpArray)
+
+            #synthesize results and update dxdy (SYNTHESIS NOT YET IMPLEMENTED)
+            actual_bbox = synthesis(attempted_bbox, bbox, frame)
             cv2.rectangle(frame, (attempted_bbox[0], attempted_bbox[1]), (attempted_bbox[0]+attempted_bbox[2], attempted_bbox[1]+attempted_bbox[3]), (0, 0, 255))
             cv2.circle(frame, attempted_center, 2, (0, 0, 255))
-            cv2.imshow("frame", frame)
-            cv2.waitKey(0)
-            # UPDATE LOCATION OF BALL IN SUBSEQUENT FRAME
-            success, bbox = tracker.update(testnpArray)
+            new_center = (actual_bbox[0]+actual_bbox[2]/2, actual_bbox[1]+actual_bbox[3]/2)
+            dxdy = tuple(np.subtract(new_center, center))
+            center = new_center
+
+            #update region that background subtraction should apply
+            if(dxdy[0] >= 0):
+                to_x = int(actual_bbox[0]+2*actual_bbox[2])
+                from_x = int(old_bbox[0]-2*old_bbox[2])
+            else:
+                to_x = int(old_bbox[0]+2*old_bbox[2])
+                from_x = int(actual_bbox[0]-2*actual_bbox[2])
+            if(dxdy[1] >= 0):
+                to_y = int(actual_bbox[1]+2*actual_bbox[3])
+                from_y = int(old_bbox[1]-2*old_bbox[3])
+            else:
+                to_y = int(old_bbox[1]+2*old_bbox[3])
+                from_y = int(actual_bbox[1]-2*actual_bbox[3])
 
       #draw tracked objects
       p1 = (int(bbox[0]), int(bbox[1]))
