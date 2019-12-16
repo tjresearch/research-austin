f7790c9bf2341509837017d987599d5445b2a7a0
austin17h <2020ahuang@tjhsst.edu>
2019-12-15 23:16:41 -0500

    weekly changes: updated initial frame algorithm, crude sythesis method

--- research-austin/code/optical_flow.0.py
+++ research-austin/code/optical_flow.0.py
@@ -5,20 +5,28 @@ import numpy as np
 #import cudamat
 #import gnumpy as np
 
-#"/C:/Users/austi/PycharmProjects/LacrosseBallTracking/ncaa_2019_semifinals_maryland_vs_virginia.mp4"# filename for lacrosse video
+#"/C:/Users/austi/PycharmProjects/LacrosseBallTracking/ncaa_2019_semifinals_maryland_vs_virginia.mp4"# filename for pro lacrosse video
+#"/C:/Users/austi/PycharmProjects/LacrosseBallTracking/game_vid.mp4" filename for high school lacrosse video
 
-#####TIMES AND DATA FOR VIDEOS
+#########TIMES AND DATA FOR VIDEOS
 #ncaa_2019_semifinals_maryland_vs_virginia.mp4 ##TITLE OF PRO LAX GAME
 #starts at frame 5700
 #game_vid.mp4 ##TITLE OF HIGH SCHOOL LAX GAME
 
 #########SAMPLE PASSES
-#bbox for ncaa frame 6300 pass: (235, 240, 10, 10) works
-#bbox for ncaa frame 7108 pass: (510, 322, 22, 22) works until caught in front of white player
-#bbox for game_vid frame 492 pass: (890, 96, 20, 10) doesnt work because it goes into the red track
-#bbox for ncaa_frame 6456 pass: (275, 185, 10, 20) works with background subtractor; regular pass that is fast and is not obscured or blends in with background
-#good starting frame for short pass is 6300 for semifinal
-#good starting frame for normal pass is 7100 for semifinal
+#bbox for ncaa frame 6456 to 6492 pass: (275, 185, 10, 20), (280, 195) works
+#bbox for ncaa frame 7108 to 7127 pass: (510, 322, 22, 22),  (521, 331) works until caught in front of white player ##works now
+#bbox for ncaa frame 6300 to 6318 pass: (230, 230, 22, 22), (241, 241) works
+#bbox for ncaa frame 6300
+#bbox for game_vid frame 493 to 514 pass: (850, 85, 20, 10), (860, 90) doesnt work because it goes into the red track, still does not work probably because faint color
+
+#########RESEARCH QUESTIONS
+#how to determine the dimensions of the bbox
+#how to treat obscurity
+
+#########RESEARCH IDEAS
+###if dxdy magnitude gets small suddenly enough, it probably means someone or something ended the pass, within the box, detect if there is a lot of non background color and from that determine if it is likely someone caught the pass
+
 def averageBGR(frame1):
     r = np.array(frame1[0:frame1.shape[0]:100, 0:frame1.shape[1]:100, 2])
     g = np.array(frame1[0:frame1.shape[0]:100, 0:frame1.shape[1]:100, 1])
@@ -41,36 +49,40 @@ def background_subtractor(frame1, average_b1, average_g1, average_r1, background
                 testnpArray1[a][j] = (255,255,255)
             else:
                 testnpArray1[a][j] = (0, 0, 0)
-
     return testnpArray1
 
-def synthesis(dxdy_bbox_input, bs_bbox_input, frame):
+def synthesis(dxdy_bbox_input, bs_bbox_input, testnpArray):
     actual_bbox = bs_bbox_input
-    """#if the two boxes are close enough, trust the background subtractor
+    tracker = None
+    #if the two boxes are close enough, trust the background subtractor
     if (abs(dxdy_bbox_input[0]-bs_bbox_input[0]) <= dxdy_bbox_input[2] and abs(dxdy_bbox_input[1]-bs_bbox_input[1]) <= dxdy_bbox_input[3]):
         actual_bbox = bs_bbox_input
     #if the two boxes are separate, trust the dxdy bbox input then need to reset the tracker (assuming pass continues)
     elif (abs(dxdy_bbox_input[0]-bs_bbox_input[0]) > dxdy_bbox_input[2] or abs(dxdy_bbox_input[1]-bs_bbox_input[1]) > dxdy_bbox_input[3]):
-        actual_bbox = dxdy_bbox_input"""
-
-    return actual_bbox
+        actual_bbox = dxdy_bbox_input
+        tracker = cv2.TrackerCSRT_create()
+        tracker.init(testnpArray, actual_bbox)
+    return (actual_bbox, tracker)
 
 def main():
     filename = input("Enter full path name to video")
+    vid_capture = cv2.VideoCapture(filename)
+    while(vid_capture.isOpened() == False):
+         filename = input("Invalid path. Enter full path name to video")
+         vid_capture = cv2.VideoCapture(filename)
+    print("Video accepted.")
+
     start_time = time.time()
 
     #DECLARE/DEFINE
-    vid_capture = cv2.VideoCapture(filename)
-    start_frame = 6456
-    end_frame = 6492
+    start_frame = 493
+    end_frame = 514
     i = start_frame
     tracker = cv2.TrackerCSRT_create()
     r = np.array(0)
     g = np.array(0)
     b = np.array(0)
-    testnpArray = np.zeros((720, 1280, 3), np.uint8)
     background_thresholder = 90
-    center = (280, 195)
     dxdy = (0, 0)
 
     #BEGIN ANALYSIS
@@ -83,31 +95,30 @@ def main():
     #print(average_b)
 
     ## INIT TRACKING FOR 1ST FRAME
-    bbox = (275, 185, 10, 20) #initial bounding box
+    bbox = (850, 85, 20, 10) #initial bounding box
+    center = (int(bbox[0]+bbox[2]/2), int(bbox[1]+bbox[3]/2))
 
     #set boundaries to try apply background subtractor (region should place bbox in the center of a 3x3 grid)
-    to_x = frame[0]
-    to_y = frame[1]
+    to_x = frame.shape[1]
+    to_y = frame.shape[0]
     from_x = 0
     from_y = 0
-    if (bbox[0] - bbox[2] > 0):
+    """if (bbox[0] - bbox[2] > 0):
         from_x = bbox[0] - bbox[2]
     if (bbox[1] - bbox[3] > 0):
         from_y = bbox[1] - bbox[3]
     if (bbox[0] + 2*bbox[2] < frame.shape[0]):
         to_x = bbox[0] + 2*bbox[2]
     if (bbox[1] + 2*bbox[3] < frame.shape[1]):
-        to_y = bbox[1] + 2*bbox[3]
+        to_y = bbox[1] + 2*bbox[3]"""
 
     ## APPLY BACKGROUND THRESHOLD ##
     testnpArray = background_subtractor(frame, average_b, average_g, average_r, background_thresholder, from_x, to_x, from_y, to_y) #0, frame.shape[0], 0, frame.shape[1])
     #cv2.rectangle(testnpArray, (from_x, from_y), (to_x, to_y), (255, 0, 0))
-    cv2.imshow("testnpArray", testnpArray)
-    cv2.waitKey(0)
+    #cv2.imshow("testnpArray", testnpArray)
+    #cv2.waitKey(0)
     elapsed_time = time.time() - start_time
     print(elapsed_time)
-    trackerType = "CSRT"
-    tracker = cv2.TrackerCSRT_create()
     tracker.init(testnpArray, bbox)
     cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[0]+bbox[2], bbox[1]+bbox[3]), (255, 0, 0))
     cv2.circle(frame, center, 2, (0, 255, 0))
@@ -125,14 +136,15 @@ def main():
 
       ## BACKGROUND SUBTRACTION
       testnpArray = background_subtractor(frame, average_b, average_g, average_r, background_thresholder, from_x, to_x, from_y, to_y)
-      cv2.rectangle(testnpArray, (from_x, from_y), (to_x, to_y), (255, 0, 0))
-      cv2.imshow("testnpArray", testnpArray)
-      cv2.waitKey(0)
+      #cv2.rectangle(testnpArray, (from_x, from_y), (to_x, to_y), (255, 0, 0))
+      #cv2.imshow("testnpArray", testnpArray)
+      #cv2.waitKey(0)
       if dxdy == (0,0): #base the first delta value off of background subtractor
             success, bbox = tracker.update(testnpArray)
             new_center = (bbox[0]+bbox[2]/2, bbox[1]+bbox[3]/2)
             dxdy = tuple(np.subtract(new_center, center))
             center = new_center
+            actual_bbox = bbox
       else: #test value of old delta plus position and goal is to weigh this greater than background subtractor
           # or if tracker gives location much more varied than previous delta, than invalidate background subtractor result
 
@@ -144,7 +156,10 @@ def main():
             success, bbox = tracker.update(testnpArray)
 
             #synthesize results and update dxdy (SYNTHESIS NOT YET IMPLEMENTED)
-            actual_bbox = synthesis(attempted_bbox, bbox, frame)
+            (actual_bbox, isNone) = synthesis(attempted_bbox, bbox, frame)
+            bbox = actual_bbox
+            if(isNone is not None):
+                tracker = isNone
             cv2.rectangle(frame, (attempted_bbox[0], attempted_bbox[1]), (attempted_bbox[0]+attempted_bbox[2], attempted_bbox[1]+attempted_bbox[3]), (0, 0, 255))
             cv2.circle(frame, attempted_center, 2, (0, 0, 255))
             new_center = (actual_bbox[0]+actual_bbox[2]/2, actual_bbox[1]+actual_bbox[3]/2)
@@ -153,21 +168,21 @@ def main():
 
             #update region that background subtraction should apply
             if(dxdy[0] >= 0):
-                to_x = int(actual_bbox[0]+2*actual_bbox[2])
+                to_x = int(actual_bbox[0]+actual_bbox[2]+3*dxdy[0])
                 from_x = int(old_bbox[0]-2*old_bbox[2])
             else:
                 to_x = int(old_bbox[0]+2*old_bbox[2])
-                from_x = int(actual_bbox[0]-2*actual_bbox[2])
+                from_x = int(actual_bbox[0]-actual_bbox[2]+3*dxdy[0])
             if(dxdy[1] >= 0):
-                to_y = int(actual_bbox[1]+2*actual_bbox[3])
+                to_y = int(actual_bbox[1]+actual_bbox[3]+3*dxdy[1])
                 from_y = int(old_bbox[1]-2*old_bbox[3])
             else:
                 to_y = int(old_bbox[1]+2*old_bbox[3])
-                from_y = int(actual_bbox[1]-2*actual_bbox[3])
+                from_y = int(actual_bbox[1]-actual_bbox[3]+3*dxdy[1])
 
       #draw tracked objects
-      p1 = (int(bbox[0]), int(bbox[1]))
-      p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+      p1 = (int(actual_bbox[0]), int(actual_bbox[1]))
+      p2 = (int(actual_bbox[0] + actual_bbox[2]), int(actual_bbox[1] + actual_bbox[3]))
       cv2.rectangle(frame, p1, p2, (255,0,0))
 
       # show frame
